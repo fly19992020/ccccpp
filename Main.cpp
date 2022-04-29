a9496501a10a4567e3769294bf216c674271e8b7
fly19992020 <93079590+fly19992020@users.noreply.github.com>
2022-04-29 12:54:58 +0800

    Fixed some formatting issues.

--- Main.cpp
+++ Main.cpp
@@ -24,13 +24,13 @@ void shitou()
 void jiandao()
 {
     if (com == 0) {
-        std::cout << "N";
+        std::cout << "N" << std::endl;
     }
     else if (com == 1) {
-        std::cout << "I";
+        std::cout << "I" << std::endl;
     }
     else if (com == 2) {
-        std::cout << "Y";
+        std::cout << "Y" << std::endl;
         num += 1;
     }
 }
@@ -39,14 +39,14 @@ void jiandao()
 void bu()
 {
     if (com == 0) {
-        std::cout << "Y";
-	num += 1;
+        std::cout << "Y" << std::endl;
+	    num += 1;
     }
     else if (com == 1) {
-	std::cout << "N";
+	    std::cout << "N" << std::endl;
     }
     else if (com == 2) {
-	std::cout << "I";
+	    std::cout << "I" << std::endl;
     }
 }
 
